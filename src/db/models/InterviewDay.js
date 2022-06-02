dbfbf716d192819aeae9114825cba2ccb2c86dc1
Jymmy Murillo <murillojymmy@gmail.com>
2022-06-02 12:51:51 -0500

    connecting qualification view

--- src/db/models/InterviewDay.js
+++ src/db/models/InterviewDay.js
@@ -11,24 +11,24 @@ const InterviewDaySchema = new Schema({
   userID: String,
   names: String,
   surname: String,
-  attendance: Boolean, //Se llena cuando se llama a lista en la reunion default: false
-  qualificationStatus: Boolean, // Indica si la calificaion se realizo con por lo menos 1 entrevistador y un observador
+  //attendance: Boolean, //Se llena cuando se llama a lista en la reunion default: false
+  //qualificationStatus: Boolean, // Indica si la calificaion se realizo con por lo menos 1 entrevistador y un observador
   //icebreakerQuestions: string[], //array de strings // ["¿Cual es tu nombre?", "¿De donde eres?", "¿Que te gusta hacer?"]
   interviewScore: Number, // score final interview => promedio de interview Score
   assesmentScore: Number, // score final assesment => promedio de assesment Score
   interviewDayScore: Number, // score final total => promedio de assesment Score y interview score
-  questionaryID: [], //array de strings // debe tener 2 questionary ids, uno para el interviewer, uno para el assesment
+  questionaryAssesmentId: String, //array de strings // debe tener 2 questionary ids, uno para el interviewer, uno para el assesment
+  questionaryInterviewersId: String,
   interviewers: [
     {
-      userID: String, // Interviewr _id
+      selectorId: String, // Interviewr _id
       names: String,
       surname: String,
-      interviewComment: String,
-      interviewerScore: Number,
-      qualificationStatus: Boolean, // Se actualiza en Enviar Evaluacion => default: false
+      comment: String,
+      score: Number,
+      //qualificationStatus: Boolean, // Se actualiza en Enviar Evaluacion => default: false
       qualifications: [
         {
-          groupID: String,
           name: String,
           score: Number, // Entre 1 y 5
         },
@@ -37,12 +37,12 @@ const InterviewDaySchema = new Schema({
   ],
   observers: [
     {
-      userID: String, // Observer _id
+      selectorId: String, // Observer _id
       names: String,
       surname: String,
-      assesmentComment: String,
-      assesmentScore: Number,
-      qualificationStatus: Boolean, // Se actualiza en Enviar => default: false
+      comment: String,
+      score: Number,
+      //qualificationStatus: Boolean, // Se actualiza en Enviar => default: false
       qualifications: [
         {
           groupID: String,
